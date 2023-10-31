b313d77b44c475343c7eafdf919e656aab0ab401
Daniel Perez <daniel.perez.olivares@gmail.com>
2023-10-31 20:24:34 -0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Daniel Perez Olivares on 31-10-23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
